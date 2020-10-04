367d5b71291b3f1b1254484aa9382d696125bcb9
tejaschauhan <tejaschauhan737@gmail.com>
2020-10-04 22:11:09 +0530

    add mongodb package requirements

--- games_of_data/games_of_data/settings.py
+++ games_of_data/games_of_data/settings.py
@@ -83,12 +83,22 @@ ASGI_APPLICATION = 'games_of_data.routing.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'flipkart',
+        'HOST': '127.0.0.1',
+        'PORT': '27017',
     }
 }
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
