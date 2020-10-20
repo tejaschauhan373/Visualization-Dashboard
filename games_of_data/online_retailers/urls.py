f152b6b9e1cb8e1d7143bb17edc2e4f6ff3ed99e
akash <akashdesai326@gmail.com>
2020-10-21 01:55:29 +0530

    merge project

--- games_of_data/online_retailers/urls.py
+++ games_of_data/online_retailers/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('brand_comparision', views.avg_price_by_ram, name="retailers")
+    path('brand_comparision/', views.avg_price_by_ram, name="retailers")
     #path('test',views.test,name="test")
 ]
