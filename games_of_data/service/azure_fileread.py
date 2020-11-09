636fcede2919475dd11c644027fa726433f97742
tejaschauhan <tejaschauhan737@gmail.com>
2020-11-09 23:31:28 +0530

    add function to upload & download from azure

--- games_of_data/service/azure_fileread.py
+++ games_of_data/service/azure_fileread.py
@@ -7,4 +7,8 @@ import os
 file_service = FileService(account_name=azure_storage_account_name,
                            account_key=azure_storage_access_key)
 
-file_service.get_file_to_path('test', None, 'myfile_from_root', os.path.join(Path(os.getcwd()).parent, 'myfile.xlsx'))
+
+def get_file_from_path(azure_share_name: str, azure_directory_name: str, azure_file_name: str,
+                       absolute_path_of_directory: str, file_name_with_extension: str):
+    file_service.get_file_to_path(azure_share_name, azure_directory_name, azure_file_name, absolute_path_of_directory,
+                                  file_name_with_extension)
