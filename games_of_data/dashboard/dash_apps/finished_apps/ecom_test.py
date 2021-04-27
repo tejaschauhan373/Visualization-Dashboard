a13e386bff1cd9cbcdd4633b6285ab5dc6f89639
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-04-27 23:31:15 +0530

    add ecom_test.py

--- games_of_data/dashboard/dash_apps/finished_apps/ecom_test.py
+++ games_of_data/dashboard/dash_apps/finished_apps/ecom_test.py
@@ -1,47 +1,93 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-from datetime import datetime
-
-df = pd.read_csv(
-    r"mobile.csv")
+from jupyter_dash import JupyterDash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    dcc.Dropdown(
-        id='demo-dropdown',
-        options=[
-            {'label': 'Redmi Note 8 Pro', 'value': 'Redmi Note 8 Pro'},
-            {'label': 'Redmi Note 9 Pro', 'value': 'Redmi Note 9 Pro'},
-            {'label': 'S1 Pro', 'value': 'S1 Pro'}
-        ],
-        value='select'
-    ),
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df_bar = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df_bar, x="Fruit", y="Amount", color="City", barmode="group", height=300, width=800)
+
+app.layout = html.Div(children=[
+    # All elements from the top of the page
     html.Div([
-        dcc.Graph(
-            id='crossfilter-indicator-scatter',
-        )
-    ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'})
-    # html.Div(id='dd-output-container')
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='demo-dropdown',
+                    options=[
+                        {'label': 'New York City', 'value': 'NYC'},
+                        {'label': 'Montreal', 'value': 'MTL'},
+                        {'label': 'San Francisco', 'value': 'SF'}
+                    ],
+                    value='NYC'
+                )
+            ], style={"width": "50%"},),
+            html.H3(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web application framework for Python.
+            '''),
+
+            dcc.Graph(
+                id='graph1',
+                figure=fig
+            ),
+        ], className='six columns'),
+        html.Div([
+            html.H3(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web application framework for Python.
+            '''),
+
+            dcc.Graph(
+                id='graph2',
+                figure=fig
+            ),
+        ], className='six columns'),
+    ], className='row'),
+    # New Div for all elements in the new 'row' of the page
+    html.Div([
+        html.Div([
+            html.H3(children='Hello Dash'),
+
+            html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+            dcc.Graph(
+                id='graph3',
+                figure=fig
+            ),
+        ], className='six columns'),
+        html.Div([
+            html.H3(children='Hello Dash'),
+
+            html.Div(children='''
+               Dash: A web application framework for Python.
+           '''),
+
+            dcc.Graph(
+                id='graph4',
+                figure=fig,
+
+            ),
+        ], className='six columns'),
+    ], className='row'),
 ])
 
-
-@app.callback(
-    dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
-def update_output(value):
-    second_df = df
-    second_df = df[df['model_name'] == value]
-    second_df["utc_time_stamp"] = [datetime.fromtimestamp(i) for i in second_df["utc_time_stamp"]]
-    print(second_df.head())
-    fig = px.line(second_df, x="utc_time_stamp", y="price")
-    return fig
-    # return 'You have selected "{}"'.format(value)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)
